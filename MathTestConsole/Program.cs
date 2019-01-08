1432c016dd554b9e843266c909cb57266d65ce25
Kamran.Behzad <kamran.behzad@oohmedia.com.au>
2019-01-08 16:16:35 +1100

    Initial commit

--- MathTestConsole/Program.cs
+++ MathTestConsole/Program.cs
@@ -1,21 +1,61 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using NsLib.Helpers;
 
 namespace MathTestConsole
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string path = @"c:\temp\MathTestConsole\";
+        private const string sinFileName = @"sin.csv";
+
+        private static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("MathTest Console");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Open csv file
+            IoHelper.CreateDirectory(path);
+            var fullFilePath = path + sinFileName;
+            TextFileHelper.OpenTextFileForWrite(fullFilePath, false);
+            // Write header
+            TextFileHelper.AppendLine("Degrees," +
+                                      "Sin," +
+                                      "Abs(Sin)," +
+                                      "1000 x Abs(Sin)," +
+                                      "1000 x Abs(Sin x2)," +
+                                      "1000 x Abs(Sin x10)");
+
+            for (int degree = 0; degree < 720; degree++)
+            {
+                var sin = Math.Sin(degree * Math.PI / 180);
+                var sinx2 = Math.Sin(2 * degree * Math.PI / 180);
+                var sinx10 = Math.Sin(10 * degree * Math.PI / 180);
+                //var cos = Math.Cos(degree*Math.PI/180);
+                Console.WriteLine($"{degree}°: Sin={sin:f4}");
+                // write data to CSV file
+                TextFileHelper.AppendLine($"{degree}," +
+                                          $"{sin}," +
+                                          $"{Math.Abs(sin)}," +
+                                          $"{1000 * Math.Abs(sin)}," +
+                                          $"{1000 * Math.Abs(sinx2)}," +
+                                          $"{1000 * Math.Abs(sinx10)}"
+                                          );
+            }
+
+            TextFileHelper.CloseWriteTextFile();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key ...");
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            GlobalHelper.ProcessStart(fullFilePath);
+
+
         }
     }
 }
